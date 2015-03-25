42977c8b5f42e2df97b5c6f5a22618a2df06ae3d
April <zhengshenyin2012@gmail.com>
2015-03-25 14:53:20 -0500

    Initial Commit

---
+++ InCameraTests/InCameraTests.swift
@@ -0,0 +1,36 @@
+//
+//  InCameraTests.swift
+//  InCameraTests
+//
+//  Created by Zhengshen Yin on 3/25/15.
+//  Copyright (c) 2015 aprilY. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class InCameraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
