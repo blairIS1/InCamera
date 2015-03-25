7bacada40e905a927f89d8fdbee066c4d538b055
April <zhengshenyin2012@gmail.com>
2015-03-25 16:47:48 -0500

    Add UIImageView to the UIVIewController

---
+++ InCamera/backgroundImageView.swift
@@ -0,0 +1,21 @@
+//
+//  backgroundImage.swift
+//  InCamera
+//
+//  Created by Zhengshen Yin on 3/25/15.
+//  Copyright (c) 2015 aprilY. All rights reserved.
+//
+
+import UIKit
+
+class backgroundImageView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
